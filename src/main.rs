67f4f40239b5c82dc03e50ca9d0c635c64e83dad
Mikko Viitamäki <mikko.vitamaki@gmail.com>
2018-11-18 19:23:17 +0200

    record MAC address

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate blurz;
+extern crate clap;
 extern crate ruuvitag;
 
 use ruuvitag::Tag;
@@ -11,6 +12,8 @@ use std::thread;
 use blurz::bluetooth_adapter::BluetoothAdapter as Adapter;
 use blurz::bluetooth_device::BluetoothDevice as Device;
 use blurz::bluetooth_discovery_session::BluetoothDiscoverySession as DiscoverySession;
+
+use clap::{Arg, App};
 
 fn discover_tags() -> Result<Tag, Box<Error>> {
     let adapter: Adapter = try!(Adapter::init());
@@ -25,18 +28,22 @@ fn discover_tags() -> Result<Tag, Box<Error>> {
         
         'device_loop: for d in devices {
             let device = Device::new(d.clone());
-            let vendor_data = device.get_manufacturer_data().unwrap();
-            if vendor_data.contains_key(&0x0499) {
-                let tag = Tag::new(vendor_data).unwrap();
+            match device.get_manufacturer_data(){  
+                Err(_e) => println!("No manufacturer data"),
+                Ok(vendor_data) => {
+                if vendor_data.contains_key(&0x0499) {
+                let mut tag = Tag::new(vendor_data).unwrap();
+                tag.mac = Some(device.get_address().unwrap());
                 println!("Temperature {:?}", tag.temperature);
                 println!("Humidity {:?}", tag.humidity);
                 println!("Pressure {:?}", tag.pressure);
                 println!("Battery Voltage {:?}", tag.battery_voltage);
                 println!("Accelaration x:{:?} y:{:?} z:{:?}", tag.acceleration.x, tag.acceleration.y, tag.acceleration.z);
-
+                println!("MAC Address: {:?}", tag.mac.unwrap());
                 println!("===============================================");
-                Tag::post_json("http://192.168.1.4:8080/mjsonrust", tag);
+                
             }
+                }}
 
             try!(adapter.remove_device(device.get_id()));
         }
@@ -45,6 +52,18 @@ fn discover_tags() -> Result<Tag, Box<Error>> {
 }
 
 fn main() {
+
+    let _matches = App::new("App to read RuuviTag advertisements")
+                            .version("0.1.0")
+                            .author("Mikko Viitamäki")
+                            .about("Reads RuuviTag advartisement packets. Main idea was to use RPi3 as a gateway and send results over the net.")
+                            .arg(Arg::with_name("url")
+                                .short("u")
+                                .long("url")
+                                .value_name("url")
+                                .help("Send Tag data over network to given url")
+                                .takes_value(true))
+                            .get_matches();
     match discover_tags() {
         Ok(tag) => println!("Temperature: {:?}", tag),
         Err(e) => println!("Error {:?}", e),
